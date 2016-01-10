6def456582b2dfd8021eaa284c11ae0b41672983
Jason Lovell <jason@zoomcc.com>
2016-01-10 14:10:57 -0600

    fix git hash refresh check to parse the result

--- js/version/version.js
+++ js/version/version.js
@@ -1,5 +1,5 @@
 var version = {
-	updateInterval: 10000,
+	updateInterval: 60000,
 	intervalId: null
 }
 
@@ -13,7 +13,7 @@ version.checkVersion = function () {
 		url: 'controllers/hash.php',
 		success: function (data) {
 			// The githash variable is located in index.php
-			if (data && data.gitHash !== gitHash) {
+			if (data && JSON.parse(data).gitHash !== gitHash) {
 				window.location.reload();
 				window.location.href = window.location.href;
 			}
